d521c112c6c2e5ab595057fe6c4dd69f79ad64e4
ken01234 <119032685+ken01234@users.noreply.github.com>
2022-12-14 13:11:18 +0800

    Update Find repeating character.cpp

--- Find repeating character.cpp
+++ Find repeating character.cpp
@@ -3,9 +3,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 char repeat(string str){//Find repeating character
-  vector<char>remain={};
+  vector<char>remain={};//stores the character that has been found 
   for(char c :str){
-if(find(remain.begin(),remain.end(),c)!=remain.end()){
+if(find(remain.begin(),remain.end(),c)!=remain.end()){// checks whether the character is in the vector
   return c;
     }else{
       remain.push_back(c);
@@ -14,46 +14,7 @@ if(find(remain.begin(),remain.end(),c)!=remain.end()){
 return '\0';
 }
 
-
-int duplicate(vector<int>n1){
-  vector<int>ans={};
-  sort(n1.begin(),n1.end());
-    for(int i=0;i<n1.size();i++){
-      if(n1[i]!=n1[i+1]){
-        ans.push_back(n1[i]);
-    }
-  } 
-  for(auto a:ans){
-    cout<<a<<endl;
-  }
-  return 0;
-}
 int main() {
-// vector<int> num ={4, 5, 1, -3, 6};
-// int k=8;
-// int p=4;
-// int n=0;
-//   sort(num.begin(),num.end());
-// while(n<p){
-//   if (num[n]+num[p]==k){
-//     cout<<true;
-//     break;
-// } if(num[n]+num[p]<k){
-//     n++;
-//     cout<<"j"<<endl;
-//   }if(p==0){
-//     n++;
-//     p=4;
-// }if(num[n]+num[p]>k) {
-//     p--;
-//     cout<<"K"<<endl;
-// }
-// }if(num[n]+num[p]!=k){
-//     cout<<false;
-//   }
-  
-  // cout<< repeat("abba");
-  
-  duplicate({4, 4, 2, 3, 2, 2, 4, 3});
+cout<< repeat("abba");
   }
   
