145d7a00606517549839c471ff429304e7feb850
ken01234 <119032685+ken01234@users.noreply.github.com>
2022-12-14 13:13:59 +0800

    Update Remove duplicates.cpp

--- Remove duplicates.cpp
+++ Remove duplicates.cpp
@@ -2,25 +2,13 @@
 #include <vector>
 #include <bits/stdc++.h>
 using namespace std;
-char repeat(string str){//Find repeating character
-  vector<char>remain={};
-  for(char c :str){
-if(find(remain.begin(),remain.end(),c)!=remain.end()){
-  return c;
-    }else{
-      remain.push_back(c);
-    }
-    }
-return '\0';
-}
-
 
 int duplicate(vector<int>n1){
   vector<int>ans={};
   sort(n1.begin(),n1.end());
     for(int i=0;i<n1.size();i++){
       if(n1[i]!=n1[i+1]){
-        ans.push_back(n1[i]);
+        ans.push_back(n1[i]);//stores the number if not in the vector
     }
   } 
   for(auto a:ans){
@@ -29,31 +17,7 @@ int duplicate(vector<int>n1){
   return 0;
 }
 int main() {
-// vector<int> num ={4, 5, 1, -3, 6};
-// int k=8;
-// int p=4;
-// int n=0;
-//   sort(num.begin(),num.end());
-// while(n<p){
-//   if (num[n]+num[p]==k){
-//     cout<<true;
-//     break;
-// } if(num[n]+num[p]<k){
-//     n++;
-//     cout<<"j"<<endl;
-//   }if(p==0){
-//     n++;
-//     p=4;
-// }if(num[n]+num[p]>k) {
-//     p--;
-//     cout<<"K"<<endl;
-// }
-// }if(num[n]+num[p]!=k){
-//     cout<<false;
-//   }
   
-  // cout<< repeat("abba");
-  
-  duplicate({4, 4, 2, 3, 2, 2, 4, 3});
+  duplicate({4, 4, 2, 3, 2, 2, 4, 3});// 
   }
   
